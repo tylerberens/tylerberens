8945b910733068b451b76e3dfa0ce39c2128158f
tylerberens <berens.tyler3@gmail.com>
2020-02-09 15:14:50 -0500

    Finished navigation

--- src/app/home/c/hero.jsx
+++ src/app/home/c/hero.jsx
@@ -3,6 +3,7 @@ import Layer1 from "../svg/layer1";
 import Layer2 from "../svg/layer2";
 import Layer3 from "../svg/layer3";
 import Layer4 from "../svg/layer4";
+import { Link } from "react-router-dom";
 
 export class hero extends Component {
   render() {
@@ -13,7 +14,9 @@ export class hero extends Component {
             <div className="col-sm-12 col-md-6">
               <h1>tyler berens</h1>
               <h2 className="c">developer & creative</h2>
-              <button>SEE MY WORK</button>
+              <Link to="/work" className="seeMyWork">
+                see my work
+              </Link>
             </div>
             <div className="col-sm-12 col-md-6">
               <h3 className="c fix">full-stack developer</h3>
--- src/app/nav/nav.jsx
+++ src/app/nav/nav.jsx
@@ -4,63 +4,76 @@ import { Link } from "react-router-dom";
 import Logo from "./logo";
 
 export class nav extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       x: false,
       top: true
     };
+
+    this.toggleNavClick = this.toggleNavClick.bind(this);
   }
-  componentDidMount(){
-    window.addEventListener('scroll', this.handleScroll);
+
+  toggleNavClick() {
+    this.setState(state => ({
+      x: !state.x
+    }));
   }
-  componentWillUnmount(){
-    window.removeEventListener('scroll', this.handleScroll);
+  componentDidMount() {
+    window.addEventListener("scroll", this.handleScroll);
+  }
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.handleScroll);
   }
   handleScroll = () => {
-    if(window.scrollY = 0){
-      this.setState({top: true});
-    }
-    else if(window.scrollY > 100){
-      this.setState({top: false});
+    if ((window.scrollY = 0)) {
+      this.setState({ top: true });
+    } else if (window.scrollY > 100) {
+      this.setState({ top: false });
       console.log(window.scrollY);
     }
-  }
+  };
   render() {
     return (
-      <nav id="nav" className={`navbar navbar-dark navbar-expand-lg p-4 ${this.state.top = true ? 'top' : 'not'}`} role="navigation">
+      <nav
+        id="nav"
+        className={`navbar navbar-dark navbar-expand-lg offset-xl-1 col-xl-10 p-4 ${(this.state.top = true
+          ? "top"
+          : "not")}`}
+        role="navigation"
+      >
         <div className="container-fluid">
-        <Link to="/">
-                  <Logo />
-              </Link>
-        <button className="navbar-toggler" onClick={() => this.setState({ x: !this.state.x })}>
-          <div />
-          <div />
-          <div />
-        </button>
-          <div className={this.state.x ? 'navbar-collapse fadeDown': 'offScreen navbar-collapse'} id="navbarButton">
-          <div id="backdrop"></div>
+          <Link to="/" className="logo">
+            <Logo />
+          </Link>
+          <button className="navbar-toggler" onClick={this.toggleNavClick}>
+            <div />
+            <div />
+            <div />
+          </button>
+          <div
+            className={
+              this.state.x
+                ? "navbar-collapse fadeDown"
+                : "offScreen navbar-collapse"
+            }
+            id="navbarButton"
+          >
+            <div id="backdrop"></div>
             <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
               <li className="nav-item active">
-              <Link to="/resume">
-                Resume
-              </Link>
+                <Link to="/resume" onClick={this.toggleNavClick}>
+                  Resume
+                </Link>
               </li>
-              <li className="nav-item">
-              <Link to="/work">
-                Work
-              </Link>
+              <li className="nav-item" onClick={this.toggleNavClick}>
+                <Link to="/work">Work</Link>
               </li>
-              <li className="nav-item">
-              <Link to="/blog">
-                Blog
-              </Link>
+              <li className="nav-item" onClick={this.toggleNavClick}>
+                <Link to="/blog">Blog</Link>
               </li>
-              <li className="nav-item">
-              <Link to="/about">
-                About
-              </Link>
+              <li className="nav-item" onClick={this.toggleNavClick}>
+                <Link to="/about">About</Link>
               </li>
             </ul>
           </div>
